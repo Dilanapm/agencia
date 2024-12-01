5f21050d8de442257e53738f264da61ed296f60b
Dilanapm <apolacadilan@gmail.com>
2024-12-01 19:07:17 -0400

    dilan cambios

--- src/redux/reducers/auth.js
+++ src/redux/reducers/auth.js
@@ -5,11 +5,14 @@ import {
     LOGIN_FAIL,
     LOGOUT,
 } from '../actions/types';
+// Verifica si hay un token almacenado en localStorage
+const token = localStorage.getItem('token');
+const role = localStorage.getItem('role');
 
 const initialState = {
-    isAuthenticated: false,
-    token: null, // Intenta inicializar desde localStorage
-    role: null, // Rol desde localStorage
+    isAuthenticated: !!token, // Si hay token, está autenticado
+    token: token || null, // Inicializa con el token de localStorage
+    role: role || null, // Inicializa con el rol de localStorage
 };
 
 function authReducer(state = initialState, action) {
@@ -29,7 +32,8 @@ function authReducer(state = initialState, action) {
         // cambios paraa login    
         case LOGIN_SUCCESS:
             console.log("LOGIN_SUCCESS ejecutado:", payload);
-            localStorage.setItem('role', payload.role); // Guarda el rol en localStorage
+            localStorage.setItem('token', payload.token); // Guarda el token
+            localStorage.setItem('role', payload.role); // Guarda el rol
             return {
                 ...state,
                 isAuthenticated: true,
